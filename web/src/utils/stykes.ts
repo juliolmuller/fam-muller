84c68b9d8aab2d81540894a93018044b3842d6f7
juliolmuller <juliolmuller@outlook.com>
2023-05-01 23:28:09 -0300

    feat: create utility function to merge classes

---
+++ web/src/utils/stykes.ts
@@ -0,0 +1,27 @@
+export type ClassValue =
+  | Record<string, undefined | boolean | null>
+  | ClassValue[]
+  | undefined
+  | boolean
+  | string
+  | null;
+
+export function clsx(...classValues: ClassValue[]) {
+  const classNames: string[] = [];
+
+  classValues.forEach((value) => {
+    if (!value) {
+      return;
+    } else if (typeof value === 'string' && value.trim()) {
+      classNames.push(value);
+    } else if (Array.isArray(value)) {
+      classNames.push(clsx(value));
+    } else if (typeof value === 'object') {
+      Object.entries(value).forEach(([value, condition]) => {
+        condition && classNames.push(value);
+      });
+    }
+  });
+
+  return classNames.join(' ');
+}
