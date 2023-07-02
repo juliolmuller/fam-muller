8c53f878695159312312a9cc93008d179afa3f72
juliolmuller <juliolmuller@outlook.com>
2023-07-01 23:59:55 -0300

    style: fix Button logics to apply active link styles

--- web/src/components/Button/Button.vue
+++ web/src/components/Button/Button.vue
@@ -20,13 +20,20 @@ defineSlots<ButtonSlots>();
 
 const route = useRoute();
 
+const isMatching = computed(() => {
+  const thisURL = props.to?.toString() ?? '';
+  const paths = route.matched.map((route) => route.path);
+
+  return paths.includes(thisURL);
+});
+
 const actualClasses = computed(() => {
   return clsx(
     'flex items-center justify-center h-12 rounded-md px-4 transition-colors font-bold text-center active:scale-[98%] active:filter active:brightness-[96%]',
     props.secondary
       ? 'text-brand-800 hover:text-brand-900 hover:bg-gray-100'
       : 'bg-brand-800 text-white hover:bg-brand-900',
-    props.isNav && !route.matched && 'font-normal text-gray-950 opacity-60 hover:opacity-100',
+    props.isNav && !isMatching.value && 'font-normal text-gray-950 opacity-60 hover:opacity-100',
     props.class,
   );
 });
