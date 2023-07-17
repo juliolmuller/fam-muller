15d5d79433ded9bab2d4fdd1ac1be94da5ce0331
juliolmuller <juliolmuller@outlook.com>
2023-07-17 00:30:58 -0300

    feat: create base content of TimelinePage

--- web/src/pages/TimelinePage/TimelinePage.vue
+++ web/src/pages/TimelinePage/TimelinePage.vue
@@ -1,15 +1,72 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from 'vue';
+
+import { Button } from '~/components';
+
+import { mockedEvents } from './mocks';
+
+const timelineEvents = ref(mockedEvents);
+</script>
 
 <template>
-  <div class="flex h-[60vh] flex-col justify-between lg:container">
-    <div class="text-4xl text-brand-800">Linha do Tempo (em construção)</div>
+  <div class="TimelinePage-root">
+    <div class="TimelinePage-header">
+      <h2>Linha do Tempo</h2>
 
-    <img
-      class="self-center"
-      alt="page under construction"
-      src="/img/under-construction.svg"
-      height="600"
-      width="600"
-    />
+      <Button>Adicionar Evento</Button>
+    </div>
+
+    <ul class="TimelinePage-body">
+      <li
+        v-for="event in timelineEvents"
+        :key="event.id"
+      >
+        <h3>{{ event.name }}</h3>
+      </li>
+    </ul>
   </div>
 </template>
+
+<style scoped lang="scss">
+.TimelinePage-root {
+  @apply flex h-[60vh] flex-col gap-8 lg:container;
+}
+
+.TimelinePage-header {
+  @apply sticky top-0 flex w-full items-center justify-between;
+
+  h2 {
+    @apply text-4xl text-brand-800;
+  }
+}
+
+.TimelinePage-body {
+  @apply px-8;
+
+  li {
+    @apply relative pb-8;
+
+    &::before {
+      content: '•';
+      @apply inline-block mr-8 ml-[0.06rem] text-white font-bold scale-150 -translate-y-1;
+    }
+
+    &::after {
+      content: '';
+      @apply absolute top-0 bottom-0 left-0 -z-10 w-2 bg-brand-800;
+    }
+
+    &:first-of-type::after {
+      @apply top-2 rounded-t-full;
+    }
+
+    &:last-of-type::after {
+      @apply bottom-auto h-[1.1rem] rounded-b-full;
+    }
+
+    h3 {
+      @apply inline-block text-xl;
+    }
+  }
+}
+</style>
---
+++ web/src/pages/TimelinePage/mocks.ts
@@ -0,0 +1,165 @@
+export interface User {
+  id: string;
+  name: string;
+  fullName: string;
+}
+
+export interface TimelineEventAttachment {
+  id: string;
+  name: string;
+  type: 'image' | 'link' | 'pdf' | 'video';
+  url: string;
+}
+
+export interface TimelineEvent {
+  id: string;
+  name: string;
+  startDate: Date;
+  endDate: Date | null;
+  description: string | null;
+  attachments: TimelineEventAttachment[];
+  markedPeople: User[];
+}
+
+const genRandomId = () => Math.random().toString(36).split('.')[1];
+
+const mockedUsers: User[] = [
+  {
+    id: genRandomId(),
+    name: 'Shirley',
+    fullName: 'Shirley Marvin',
+  },
+  {
+    id: genRandomId(),
+    name: 'Elisa',
+    fullName: 'Elisa Koelpin',
+  },
+  {
+    id: genRandomId(),
+    name: 'Percy',
+    fullName: 'Percy Turcotte',
+  },
+  {
+    id: genRandomId(),
+    name: 'Irvin',
+    fullName: 'Irvin Boehm DVM',
+  },
+  {
+    id: genRandomId(),
+    name: 'Alberto',
+    fullName: 'Alberto Sporer',
+  },
+  {
+    id: genRandomId(),
+    name: 'Marcos',
+    fullName: 'Marcos Schaefer',
+  },
+  {
+    id: genRandomId(),
+    name: 'Salvatore',
+    fullName: 'Salvatore Rolfson',
+  },
+  {
+    id: genRandomId(),
+    name: 'Cora',
+    fullName: 'Cora Zboncak',
+  },
+  {
+    id: genRandomId(),
+    name: 'Erma',
+    fullName: 'Erma Murphy',
+  },
+  {
+    id: genRandomId(),
+    name: 'Keith',
+    fullName: 'Keith Kreiger',
+  },
+];
+
+const getRandomUsers = (count = 1) => {
+  const usersSet = new Set<User>();
+  const totalUsers = mockedUsers.length;
+
+  while (usersSet.size < count) {
+    const randomNumber = Math.random();
+    const randomIndex = Math.round(randomNumber * totalUsers);
+
+    usersSet.add(mockedUsers[randomIndex]);
+  }
+
+  return [...usersSet];
+};
+
+const getRandomPhotos = (count = 1): TimelineEventAttachment[] => {
+  return Array(count)
+    .fill(null)
+    .map(() => ({
+      id: genRandomId(),
+      name: 'photo',
+      type: 'image',
+      url: 'https://picsum.photos/200',
+    }));
+};
+
+export const mockedEvents: TimelineEvent[] = [
+  {
+    id: genRandomId(),
+    name: 'Esse é um evento',
+    description:
+      'Corporis exercitationem ex rerum eos sunt ex. Explicabo nisi libero. Aut distinctio unde aut voluptas. Enim fuga reprehenderit rerum repudiandae quia voluptatem.',
+    startDate: new Date(1899, 5, 20),
+    endDate: null,
+    markedPeople: getRandomUsers(2),
+    attachments: getRandomPhotos(2),
+  },
+  {
+    id: genRandomId(),
+    name: 'Outro Evento',
+    description:
+      'Ipsa est nihil laboriosam nihil. Dolorum quia velit nisi dolores harum molestiae sit. Vero omnis consectetur. Quo enim velit. Doloribus aut quaerat magnam.',
+    startDate: new Date(1914, 8, 13),
+    endDate: null,
+    markedPeople: getRandomUsers(),
+    attachments: getRandomPhotos(5),
+  },
+  {
+    id: genRandomId(),
+    name: 'Evento do Século',
+    description:
+      'Voluptas deleniti et et sed modi quo. Quibusdam adipisci occaecati ipsum excepturi quod voluptates vel necessitatibus. Officia voluptatibus qui aut illo placeat. Quis architecto voluptas ducimus sint ut harum. Explicabo et vitae consequuntur non ducimus nihil corporis sit. Earum placeat qui aut sit quia animi nulla.',
+    startDate: new Date(1918, 2, 15),
+    endDate: null,
+    markedPeople: getRandomUsers(),
+    attachments: getRandomPhotos(4),
+  },
+  {
+    id: genRandomId(),
+    name: 'Que sucesso esse momento!',
+    description:
+      'Sunt impedit voluptatum eum facere eum ea qui id est. Rem nisi quis veniam dolore consequatur voluptatem ea. Est minus sed aut autem sit dolores perspiciatis eum. At et cum. Quis natus sequi. Eum libero est autem eum ut culpa doloribus.',
+    startDate: new Date(1918, 2, 15),
+    endDate: new Date(1918, 2, 16),
+    markedPeople: getRandomUsers(2),
+    attachments: [],
+  },
+  {
+    id: genRandomId(),
+    name: 'E nasce uma estrela!',
+    description:
+      'Odit neque expedita quo est eius voluptatibus vitae ad. Laboriosam quia et voluptatem. Occaecati reiciendis est ab placeat minima.',
+    startDate: new Date(1966, 2, 26),
+    endDate: null,
+    markedPeople: getRandomUsers(3),
+    attachments: getRandomPhotos(3),
+  },
+  {
+    id: genRandomId(),
+    name: 'Coisa de loco!',
+    description:
+      'Aspernatur mollitia minima et ab unde qui praesentium ipsa. Sit explicabo autem. Eveniet qui aut dicta molestias ipsa ut et nemo. Laborum sit non soluta vero deleniti non at vitae. Eos commodi repudiandae. Dicta ab dolor.',
+    startDate: new Date(1967, 4, 23),
+    endDate: null,
+    markedPeople: getRandomUsers(6),
+    attachments: getRandomPhotos(2),
+  },
+];
